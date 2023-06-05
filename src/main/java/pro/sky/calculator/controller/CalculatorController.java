05d7b48fb3d92660e4e391375871dc37f253e208
ruslan <prus666@mail.ru>
2023-06-05 03:35:20 +0300

    ДЗ мавен и спринг бут

---
+++ src/main/java/pro/sky/calculator/controller/CalculatorController.java
@@ -0,0 +1,56 @@
+package pro.sky.calculator.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.calculator.service.CalculatorService;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+    public CalculatorController (CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping
+    public String hello() {
+        return "Добро пожаловать в калькулятор.";
+    }
+    @GetMapping("/plus")
+    public String plus (@RequestParam(value = "num1", required = false) Integer a,
+            @RequestParam (value = "num2", required = false) Integer b) {
+
+        return buildView(a, b, calculatorService.plus(a,b), "+");
+    }
+    @GetMapping("/minus")
+    public String minus (@RequestParam(value = "num1", required = false) Integer a,
+                        @RequestParam (value = "num2", required = false) Integer b) {
+
+        return buildView(a, b, calculatorService.minus(a,b), "-");
+    }
+    @GetMapping("/multiply")
+    public String multiply (@RequestParam(value = "num1", required = false) Integer a,
+                         @RequestParam (value = "num2", required = false) Integer b) {
+
+        return buildView(a, b, calculatorService.multiply(a,b), "*");
+    }
+        @GetMapping("/divide")
+    public String divide (@RequestParam(value = "num1", required = false) Integer a,
+                            @RequestParam (value = "num2", required = false) Integer b) {
+
+        return buildView(a, b, calculatorService.divide(a,b), "/");
+    }
+
+    private String buildView(Integer a, Integer b, Number result, String operation){
+        if (a == null || b == null) {
+            return "Не передан один из параметров";
+        }
+        if ("/".equals(operation) && b == 0) {
+            return "На ноль делить нельзя";
+        }
+        return a + " " + operation + " " + b + " = " + result;
+
+    }
+}
